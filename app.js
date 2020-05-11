62eaba903ee58920e23efb805b5ad7bd256cc32b
linnan1999 <43462603+linnan1999@users.noreply.github.com>
2020-05-11 21:54:18 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    appid: '',//appid需自己提供，此处的appid我随机编写 wx0b62f800278d2aa7
+    secret: '',//secret需自己提供，此处的secret我随机编写 ad04e61e0e242ad744f3400c4c75d5bf
+    Authorization: '',
+    phoneNum: '',
+    formId: '',
+    Id: '',
+    OpenId: '',
+    IndentCode: '',
+    UserId:'',
+    userInfo: null,
+    orders: []
+  }
+  
+})
